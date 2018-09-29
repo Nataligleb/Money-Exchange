1246bbca02b0d0d0008b9fedbe08aeddc136a90e
name <nata.mozhaueva@gmail.com>
2018-09-30 02:22:22 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+	if(currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+	if(currency === 0) return {}; 
+	const result = {};
+	const arrcoin = [{name:'H', coin:50}, {name:'Q', coin:25}, {name:'D', coin:10}, {name:'N', coin:5}, {name:'P', coin:1}];
+
+	for(let i = 0; currency>0; i++){
+		let thiscoin = arrcoin[i].coin;
+		let thiscoiname = arrcoin[i].name;
+		if(currency >= thiscoin){
+			result[thiscoiname] = Math.floor(currency/thiscoin);
+			currency = currency % thiscoin;
+		}
+	}
+	return result;
 }
